1fb20bf9ba80156d59d56ed3bf63aa5d31c56f5a
Andrea Mancini <mancioman2@gmail.com>
2019-07-25 13:04:14 +0200

    new

--- src/main/java/ThNew.java
+++ src/main/java/ThNew.java
@@ -4,6 +4,7 @@
  */
 
 
+import java.util.Random;
 
 public class ThNew extends Thread {
 
@@ -12,16 +13,19 @@ public class ThNew extends Thread {
     private TxColor tc;
 
     ThNew(String name){
-        name = this.name;
+        this.name = name;
         tc = new TxColor();
     }
 
     @Override
     public void run(){
+        try{
+            sleep(new Random().nextInt(1000));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
         System.out.println(tc.colPick()+"Thread " + name + " started");
     }
-
-
-
 
 }
--- src/main/java/ThRandy.java
+++ src/main/java/ThRandy.java
@@ -2,12 +2,24 @@
     Simple program to study concurrency
  */
 
+import java.util.Random;
+
 public class ThRandy {
 
     public static void main(String[] args) {
 
-        ThNew th1 = new ThNew("thread 1");
+        Random r = new Random();
+
+        ThNew th1 = new ThNew("Mario");
+        ThNew th2 = new ThNew("Giorgio");
+        ThNew th3 = new ThNew("Andrea");
         th1.start();
+
+
+
+
+        th2.start();
+        th3.start();
 
     }
 
