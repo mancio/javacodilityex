97c4204bc9e7ea614314afc12f0ea027a938afbe
Andrea Mancini <mancioman2@gmail.com>
2019-11-08 16:50:07 +0100

    new

---
+++ src/main/java/MultiStream.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class MultiStream {
+    public static void main(String[] args) {
+        Random ran = new Random();
+
+        int[] ar1 = new int[10];
+        int[] ar2 = new int[10];
+
+        for(int i=0; i<10; i++){
+            ar1[i] = ran.nextInt(10);
+            ar2[i] = ran.nextInt(10);
+            System.out.println(ar1[i] + " " + ar2[i]);
+        }
+
+        System.out.println(sol(ar1,ar2));
+
+    }
+
+    static List sol(int[] ar1, int[] ar2){
+
+        List<Integer> tot = IntStream.range(0, ar1.length).mapToObj(i->compare(ar1[i],ar2[i])).collect(Collectors.toList());
+        return tot;
+    }
+
+    static int compare(int a, int b){
+        if(a>=b){return a;}
+        return b;
+    }
+
+}
