527d0a22200b16ca2318353fc57fc4d0bea36096
andrea.mancini <mancioman2@gmail.com>
2019-11-11 15:01:55 +0100

    new

--- src/main/java/Matrix.java
+++ src/main/java/Matrix.java
@@ -37,36 +37,57 @@ public class Matrix {
                 {9,1,2}
         };
 
+        int[][] B = new int[][]{
+                {3},
+                {4},
+                {8}
+        };
 
+        int[][] C = new int[][]{
+                {3,9,0}
+
+        };
 
         System.out.println(sol(A));
 
     }
 
-    static String sol(int[][] A){
+    static String sol(int[][] A) {
 
-        List<Integer> l = new LinkedList<>();
-        int v = 0;
-        int h = 0;
-        l.add(A[0][0]);
-        while (v<A.length-1){
+        String st = "";
 
-            if(h==A[0].length-1){
-                l.add(A[v+1][h]);
-                v++;
-            } else if(A[v+1][h]>=A[v][h+1]){
-                l.add(A[v+1][h]);
-                v++;
-            }else {
-               l.add(A[v][h+1]);
-               h++;
+        int v = A.length - 1;
+        int h = A[0].length - 1;
+        if (h == 0) {
+            for (int i = 0; i <= v; i++) {
+                st = st.concat(String.valueOf(A[i][0]));
+            }
+        } else if (v == 0) {
+            for (int i = 0; i <= h; i++) {
+                st = st.concat(String.valueOf(A[0][i]));
             }
 
+        } else {
+
+            st = st.concat(String.valueOf(A[0][0]));
+
+            int m = 0; //vertical
+            int n = 0; // horizontal
+            while(m<v && n<h) {
+                if(A[m+1][n]>=A[m][n+1]){
+                    st = st.concat(String.valueOf(A[m+1][n]));
+                    m++;
+                }else {
+                    st = st.concat(String.valueOf(A[m][n+1]));
+                    n++;
+                }
+
+            }
+
+            st = st.concat(String.valueOf(A[v][h]));
         }
 
-        String st = l.stream().map(i->String.valueOf(i)).collect(Collectors.joining());
 
         return st;
     }
-
 }
