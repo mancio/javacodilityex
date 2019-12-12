0086e458b06d7079518e9e9343f34140b658f70b
Andrea Mancini <mancioman2@gmail.com>
2019-12-12 15:22:02 +0100

    new

--- src/main/java/CountingValleys.java
+++ src/main/java/CountingValleys.java
@@ -8,50 +8,46 @@ import java.util.ListIterator;
 public class CountingValleys {
     public static void main(String[] args) {
 
-        int n = 8;
-        String s = "UDDDUDDU";
+        int n = 12;
+        String s = "DDUUDDUDUUUD";
 
+        //   U D  D  D  U  D  D  U
+        //  +1;0;-1;-2;-1;-2;-3;-2
+        //  1 valley
+
+        //   D  D  U  U  D  D  U  D  U  U  U  D
+        //  -1;-2;-1; 0;-1;-2;-1;-2;-1; 0;+1; 0
+        //  2 valleys
         System.out.println(countingValleys(n,s));
 
     }
 
     static int countingValleys(int n, String s) {
 
-        int up = 0;
-        int down = 0;
+        int see_level = 0;
+        int valleys = 0;
+        boolean up = true;
 
-        int valley = 0;
-
-        char old = 'r';
-
-        for (int i = 0; i<n; i++){
-            char nw = s.charAt(i);
-            if(nw=='D'&& nw==old){
-                down++;
-            }else if(nw=='U'){
-                up++;
-                if(up>1){
-
+        for(int i = 0; i<n; i++){
+            if (s.charAt(i) == 'U') {
+                see_level++;
+                if(see_level==0){
+                    up = true;
+                }
+            }else{
+                see_level--;
+                if(see_level==-1 && up){
+                    valleys++;
+                    up=false;
                 }
             }
-
-            old = nw;
         }
 
-
-        return 7;
+        return valleys;
 
     }
 
 
 }
 
-/*
-
-0 1
-1 3
-2 1
-3 2
-1 1
-
- */
+// PASS!!! :) Be careful one valley is just one step under see level (-1) not two (-2)
