72f1552e7d0c3d8b34fd2848b585e5610be6cd67
Andrea Mancini <mancioman2@gmail.com>
2019-12-13 18:33:55 +0100

    new

---
+++ src/main/java/SecMax.java
@@ -0,0 +1,26 @@
+/*
+given an array of numbers output the second maximum number of
+the array
+*/
+
+
+public class SecMax {
+
+    public static void main(String[] args) {
+
+        int a[] = {4,5,2,8,2,1,9,74};
+
+
+
+    }
+
+    static int secMax(int[] a){
+
+        int last = Integer.MIN_VALUE;
+        int seclast = last-1;
+
+        for(int i = 0; i<a.length; i++){
+            if(a[i])
+        }
+    }
+}
