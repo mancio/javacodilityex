f2020de41842cdb68810a901e59aac080ba7da26
andrea.mancini <mancioman2@gmail.com>
2019-12-14 18:47:58 +0100

    new

--- src/main/java/SecMax.java
+++ src/main/java/SecMax.java
@@ -10,17 +10,26 @@ public class SecMax {
 
         int a[] = {4,5,2,8,2,1,9,74};
 
-
+        System.out.println(secMax(a));
 
     }
 
     static int secMax(int[] a){
 
-        int last = Integer.MIN_VALUE;
-        int seclast = last-1;
+        int seclast = Integer.MIN_VALUE;
+        int last = seclast+1;
+
 
         for(int i = 0; i<a.length; i++){
-            if(a[i])
+            if()
+
         }
+        return seclast;
     }
 }
+
+/*
+
+
+
+*/
