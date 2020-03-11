f32ebda8cd6700a939042e46870e437014cb367c
Andrea Mancini <mancioman2@gmail.com>
2020-03-11 21:26:41 +0100

    new

--- src/main/java/NewYearChaos.java
+++ src/main/java/NewYearChaos.java
@@ -1,4 +1,19 @@
 // https://www.hackerrank.com/challenges/new-year-chaos/
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class NewYearChaos {
+    public static void main(String[] args) {
+        //1,2,3,4,5
+        int[] q = {2,1,5,3,4};
+        System.out.println(chaos(q));
+    }
+
+    static List chaos(int[] ar){
+        int ds = 0;
+        for(int i=ar.length-1;i>=0;i--){
+            
+        }
+    }
 }
